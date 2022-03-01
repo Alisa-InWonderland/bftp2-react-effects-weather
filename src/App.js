cf3dbb8c19ec0c25f936446f41dc92e7a60ca34a
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-03-01 12:08:51 +0100

    eventos click y keydown

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ import {useEffect, useState} from "react";
 function App() {
     const WEATHER_API = "https://weatherdbi.herokuapp.com/data/weather/";
 
-    const [count, setCount] = useState(0);
-    const [drawing, setDrawing] = useState("");
+    // const [count, setCount] = useState(0);
+    // const [drawing, setDrawing] = useState("");
     const [title, setTitle] = useState("");
     const [icon, setIcon] = useState("");
     const [cityName, setCityName] = useState("barcelona");
 
-    function updateDrawing() {
-        if (count === 0) {
-            setDrawing("✨");
-        } else {
-            setDrawing("⭐️".repeat(count));
-        }
-    }
+    // function updateDrawing() {
+    //     if (count === 0) {
+    //         setDrawing("✨");
+    //     } else {
+    //         setDrawing("⭐️".repeat(count));
+    //     }
+    // }
 
     function updateWeatherData( data ) {
         const temperature = data.currentConditions.temp.c
         const iconURL = data.currentConditions.iconURL
-        setTitle(`Buenos días, hoy tenemos ${temperature} grados en ${cityName}`)
+        const dia = data.currentConditions.dayhour
+        setTitle(`Buenos días, hoy es ${dia} tenemos ${temperature} grados en ${data.region}`)
         setIcon(iconURL)
     }
 
@@ -32,24 +33,36 @@ function App() {
             .then( updateWeatherData )
     }, [cityName]);
 
-    useEffect( () => console.log(
-        "Hola, llevamos "+ count),
-        [count]);
 
-    useEffect(updateDrawing, [count]);
 
-    function increaseCounter() {
-        setCount(count + 1);
+    // useEffect( () => console.log(
+    //     "Hola, llevamos "+ count),
+    //     [count]);
+    //
+    // useEffect(updateDrawing, [count]);
+    //
+    // function increaseCounter() {
+    //     setCount(count + 1);
+    // }
+    // onChange={(e) => setCityName(e.target.value) }
+    //
+    const ChangeCity = (event) => {
+        if (event.key === 'Enter') {
+            setCityName(document.querySelector('.input').value)
+        }
     }
 
+
+
     return (
-        <div className="App">
+        <div onKeyDown={ChangeCity} className="App">
             <h2>{ title }</h2>
-            <img src={icon} alt="weather icon"/>
-            <input type={"text"} onChange={(e) => setCityName(e.target.value) } />
-            <p> {`La cuenta es ${count}`}</p>
-            <p> { drawing } </p>
-            <button onClick={increaseCounter}>Incrementar</button>
+            <img src={icon} alt="weather icon"/> <br/>
+            <input className='input' type={"text"}  />
+            {/*<p> {`La cuenta es ${count}`}</p>*/}
+            {/*<p> { drawing } </p>*/}
+            {/*<button onClick={increaseCounter}>Incrementar</button>*/}
+            <button onClick={() => setCityName(document.querySelector('.input').value) } >Cambiar la ciudad</button>
         </div>
     );
 }
