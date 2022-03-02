da77a52ac128b2cf298827949997c69a8e9dfd9e
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-03-02 18:45:00 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -23,15 +23,25 @@ function App() {
         const temperature = data.currentConditions.temp.c
         const iconURL = data.currentConditions.iconURL
         const dia = data.currentConditions.dayhour
-        setTitle(`Buenos días, hoy es ${dia} tenemos ${temperature} grados en ${data.region}`)
+        setTitle(`Buenos días, hoy es ${dia}, tenemos ${temperature} grados en ${data.region}`)
         setIcon(iconURL)
     }
 
-    useEffect( () => {
+    const getDataByCity = () => {
         fetch(WEATHER_API + cityName) // saco el JSON de datos
             .then(r => r.json()) //
             .then( updateWeatherData )
+    }
+
+    useEffect( () => {
+        getDataByCity();
+            //.then(removeCity)
     }, [cityName]);
+
+    // useEffect( () => {setInterval(function() {
+    //     getDataByCity();
+    // }, 60000);
+    // }, []);
 
 
 
@@ -45,24 +55,34 @@ function App() {
     //     setCount(count + 1);
     // }
     // onChange={(e) => setCityName(e.target.value) }
-    //
-    const ChangeCity = (event) => {
+
+    let input = document.querySelector('.input');
+
+    const changeCity = () => {
+        setCityName(input.value)
+        input.value = "";
+    }
+
+    const onEnterPressed = (event) => {
         if (event.key === 'Enter') {
-            setCityName(document.querySelector('.input').value)
+            changeCity();
         }
     }
 
+    // const removeCity = () => {
+    //         input.value = "";
+    //     }
 
 
     return (
-        <div onKeyDown={ChangeCity} className="App">
+        <div onKeyDown={onEnterPressed} className="App">
             <h2>{ title }</h2>
             <img src={icon} alt="weather icon"/> <br/>
             <input className='input' type={"text"}  />
             {/*<p> {`La cuenta es ${count}`}</p>*/}
             {/*<p> { drawing } </p>*/}
             {/*<button onClick={increaseCounter}>Incrementar</button>*/}
-            <button onClick={() => setCityName(document.querySelector('.input').value) } >Cambiar la ciudad</button>
+            <button onClick={changeCity}>Cambiar la ciudad</button>
         </div>
     );
 }
